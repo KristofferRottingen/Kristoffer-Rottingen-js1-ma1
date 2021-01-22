65f8afc3137f7c83637aae22ee6dc00f5e72328a
KristofferRottingen <kriroe06@gmail.com>
2021-01-22 16:07:56 +0100

    d

---
+++ js/script.js
@@ -0,0 +1,44 @@
+// question 1
+const cat = {
+	complain: function () {
+		console.log("Moew!")
+	}
+};
+
+// question 2
+const heading = document.querySelector("h3");
+
+heading.innerHTML = "Updated heading";
+
+console.log(heading.innerHTML);
+
+// question 3
+heading.style = "font-size: 2em";
+
+
+//question 4
+heading.className = ("subheading");
+
+console.log(heading.className);
+
+
+// uestion 5 
+const paragraphs = document.querySelectorAll("p");
+
+for (let i = 0; i < paragraphs.length; i++) {
+
+	paragraphs[i].style.color = "red";
+};
+
+
+// question 6 
+const resultsContainer = document.querySelector(".result");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+
+resultsContainer.style.backgroundColor = "yellow";
+
+
+// question 7
+
+
